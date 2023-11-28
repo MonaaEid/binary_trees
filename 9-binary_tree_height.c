7b751716c501fe8ed73f1d86abf7782182e39244
MonaaEid <monaa_eid@hotmail.com>
2023-11-29 01:48:37 +0200

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,19 +14,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left)
-		left_height = binary_tree_height(tree->left);
-	else
-		left_height = 0;
-	if (tree->right)
-		right_height = binary_tree_height(tree->right);
-	else
-		right_height = 0;
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	if (left_height > right_height)
-		return (left_height + 1);
+		return (left_height);
 	else
-		return (right_height + 1);
+		return (right_height);
 }
 
 
