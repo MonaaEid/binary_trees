17736498fb0521b82e2939cc1b3df4af4db03e2d
MonaaEid <monaa_eid@hotmail.com>
2023-11-29 00:14:54 +0200

    update

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * binary_tree_height - function that creates a binary tree node
+ * @tree: pointer to the parent node of the node to create
+ * Return: return a pointer to the new node, or NULL on failure
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
+
+
