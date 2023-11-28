74970976984c2a514163f2eb63e4987979c43f0e
MonaaEid <monaa_eid@hotmail.com>
2023-11-28 22:21:33 +0200

    update

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,18 +3,18 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * binary_tree_preorder - function that creates a binary tree node
+ * binary_tree_inorder - function that creates a binary tree node
  * @tree: pointer to the parent node of the node to create
  * @func: pointer to the parent node of the node to create
  * Return: return a pointer to the new node, or NULL on failure
  *
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 {
 	if (tree == NULL || func == NULL)
 		return;
 
-	func(tree->n);
 	binary_tree_preorder(tree->left, func);
+	func(tree->n);
 	binary_tree_preorder(tree->right, func);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,18 +3,19 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * binary_tree_preorder - function that creates a binary tree node
+ * binary_tree_postorder - function that creates a binary tree node
  * @tree: pointer to the parent node of the node to create
  * @func: pointer to the parent node of the node to create
  * Return: return a pointer to the new node, or NULL on failure
  *
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 {
 	if (tree == NULL || func == NULL)
 		return;
 
-	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
+	func(tree->n);
 }
+
