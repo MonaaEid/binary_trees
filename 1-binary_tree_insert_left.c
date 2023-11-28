56b8dcad0077a854750da265cd0a1f1ad3669093
MonaaEid <monaa_eid@hotmail.com>
2023-11-28 17:43:49 +0200

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,14 +11,19 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left = malloc(sizeof(binary_tree_t));
-
-	if (new_left == NULL || parent == NULL)
-		return(NULL);
+	if (parent == NULL)
+        return (NULL);
+	// if (new_left == NULL || parent == NULL)
+	// 	return(NULL);
 	new_left->n = value;
 	new_left->parent = parent;
+	if (new_left == NULL)
+        return (NULL);
 	if (parent->left != NULL)
 	{
 		new_left->left = parent->left;
+		/*It sets the parent of the old left child of the parent to the new node,
+		by setting the field parent->left->parent to the pointer new_node.*/
 		parent->left->parent = new_left;
 	}
 	parent->left = new_left;
