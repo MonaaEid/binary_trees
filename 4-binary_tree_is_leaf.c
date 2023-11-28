651f0ea264b3a09055da08afff3c2ae33fb07819
MonaaEid <monaa_eid@hotmail.com>
2023-11-28 20:08:59 +0200

    update

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * binary_tree_is_leaf - function that creates a binary tree node
+ * @node: pointer to the parent node of the node to create
+ * Return: return a pointer to the new node, or NULL on failure
+ *
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node->left && node->right)
+		return (1)
+	else
+		return (0)
+}
+
