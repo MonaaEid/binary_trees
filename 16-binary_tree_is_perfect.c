8ce6b66f242f4be2f33405c0a9c454cdd85cea75
MonaaEid <monaa_eid@hotmail.com>
2023-11-30 00:59:47 +0200

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,7 +13,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (!tree->left && !tree->right)
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	if (tree->left && tree->right)
@@ -21,6 +23,26 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 			binary_tree_is_perfect(tree->right));
 
 	return (0);
+}
+/**
+ * binary_tree_height - function that creates a binary tree node
+ * @tree: pointer to the parent node of the node to create
+ * Return: return a pointer to the new node, or NULL on failure
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
 
 
