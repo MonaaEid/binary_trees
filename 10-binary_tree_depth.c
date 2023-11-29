72f8b8be4466970de446bbe9e8d651b9f25932f2
MonaaEid <monaa_eid@hotmail.com>
2023-11-29 02:37:13 +0200

    update

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -17,9 +17,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	while (tree-> parent != NULL)
 		{
 			depth++;
-			tree = tree>parent;
+			tree = tree->parent;
 		}
-	return(depth);
+	return (depth);
 
 }
 
