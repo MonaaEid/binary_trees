df5340fc32a869260ddc35282d50dd8155c81f78
MonaaEid <monaa_eid@hotmail.com>
2023-11-29 18:24:37 +0200

    update

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * binary_tree_size - function that creates a binary tree node
+ * @tree: pointer to the parent node of the node to create
+ * Return: return a pointer to the new node, or NULL on failure
+ *
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
+		return (0);
+	else
+		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
+
+
+
