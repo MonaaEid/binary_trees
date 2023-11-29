437337264845846f1bd0b393b1c4b5a22e3bd2b3
MonaaEid <monaa_eid@hotmail.com>
2023-11-29 19:14:09 +0200

    update

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,7 +15,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	else if ((tree->left != NULL) || (tree->right != NULL))
 		return (1);
 	else
-		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+		return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
 }
 
 
