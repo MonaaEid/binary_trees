be28b311e57a7207df913d6b68e0e11d2ceef14a
MonaaEid <monaa_eid@hotmail.com>
2023-11-29 20:27:12 +0200

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,10 +15,28 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	left_height = tree->left ? 1 + binary_tree_balance(tree->left) : 1;
-	right_height = tree->right ? 1 + binary_tree_balance(tree->right) : 1;
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (left_height - right_height);
 }
 
+/**
+ * binary_tree_height - function that creates a binary tree node
+ * @tree: pointer to the parent node of the node to create
+ * Return: return a pointer to the new node, or NULL on failure
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height = 0;
 
+	if (tree == NULL)
+		return (0);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
+}
