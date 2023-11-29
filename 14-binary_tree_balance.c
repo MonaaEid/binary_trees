24798340e0157ea975935a8581283ba30b9dc409
MonaaEid <monaa_eid@hotmail.com>
2023-11-29 20:14:43 +0200

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,8 +15,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	left_height = tree->left ? binary_tree_balance(tree->left) : -1;
-	right_height = tree->right ? binary_tree_balance(tree->right) : -1;
+	left_height = tree->left ? 1 + binary_tree_balance(tree->left) : 1;
+	right_height = tree->right ? 1 + binary_tree_balance(tree->right) : 1;
 	return (left_height - right_height);
 }
 
