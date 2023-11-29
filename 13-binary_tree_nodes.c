1d86b46e54b8f0df35ab43a7b2ed292df9401e93
MonaaEid <monaa_eid@hotmail.com>
2023-11-29 19:11:10 +0200

    update

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,15 +8,16 @@
  * Return: return a pointer to the new node, or NULL on failure
  *
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	else if ((tree->left == NULL) && (tree->right == NULL))
+	else if ((tree->left != NULL) || (tree->right != NULL))
 		return (1);
 	else
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
+
 
 
 
